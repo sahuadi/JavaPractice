792b03ca115a8eaecb9e0713c5c454215298626e
Aditya <aditya@paymonk.com>
2024-07-29 20:16:09 +0530

    All JAVA practice Commit

---
+++ src/p1/AddTwoNumber.java
@@ -0,0 +1,14 @@
+package p1;
+
+public class AddTwoNumber {
+
+	public static void main(String[] args) {
+		int x = 10;
+		double y = 3.57776;
+		double z = x+y;
+		double a =Math.round(z);
+		System.out.println(a);	
+
+	}
+
+}
---
+++ src/p1/SimpleInterest.java
@@ -0,0 +1,16 @@
+package p1;
+
+public class SimpleInterest {
+
+	public static void main(String[] args) {
+		int Simple_Interest = 0;
+		int Principle_Amount = 100;
+		int Rate_Of_Interest = 10;
+		int Time_Period      = 5;
+		Simple_Interest = (Principle_Amount*Rate_Of_Interest*Time_Period)/100;
+		System.out.println(Simple_Interest);
+		
+
+	}
+
+}
